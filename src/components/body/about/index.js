d2c4ea87ed2f53ecf20c05aa97240ce34a77d00c
Kholid Anas A <amrulloh30@gmail.com>
2023-07-26 00:23:02 +0700

    rollback to blue

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./about.css";
 import SocialContact from "../../common/social-contact/index";
-import { Typography } from 'antd';
 
 function About() {
-  const { Text } = Typography;
   return (
     <div className="about">
       <div className="about-top">
