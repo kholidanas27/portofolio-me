a79f8757bfbfdcd567ba48a8073caf19e29ecc86
Kholid Anas A <amrulloh30@gmail.com>
2023-07-26 00:15:44 +0700

    update base color

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./about.css";
 import SocialContact from "../../common/social-contact/index";
+import { Typography } from 'antd';
+
 function About() {
+  const { Text } = Typography;
   return (
     <div className="about">
       <div className="about-top">
         <div className="about-info">
         Hello There ✋,I am,
          <br /> <span className="info-name">Kholid Anas Amrulloh</span>.
-         <br /> I am Fullstack Developer with experience working using Laravel, Java, AngularJs, ReactJs, ExpressJs, Symfony and VueJs
+         <br /> <span>I am Fullstack Developer with experience working using Laravel, Java, AngularJs, ReactJs, ExpressJs, Symfony and VueJs</span>
         </div>
         <div className="about-photo">
           <img 
