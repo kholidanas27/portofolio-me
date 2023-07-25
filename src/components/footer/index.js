a7c15fd155a62b0a1da7b8f3a37d9ba18d90ba8e
Kholid Anas A <amrulloh30@gmail.com>
2023-07-26 00:48:30 +0700

    change love color

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">Made With ❤️</div>;
+  return <div className="footer">Made With 🖤</div>;
 }
 
 export default Footer;
